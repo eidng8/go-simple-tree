074377ff67df2a43678737a3710d637f685d86c2
eidng8 <jackey.cheung@qq.com>
2024-11-20 15:23:44 +0800

    fix bash script

--- docker_test.sh
+++ docker_test.sh
@@ -2,19 +2,21 @@
 
 # build test container
 if [ -z "$(docker images -q go-simple-tree-test 2>&1 >/dev/null)" ]; then
-  docker build -t go-simple-tree-test .
+docker build -t go-simple-tree .
 fi
 
 # setup test container
-docker run --rm --name gstt -dv ".:/root/app:rw" go-simple-tree-test
-docker exec -itu root gstt /usr/local/go/bin/go test -C /root/app -tags jsoniter
-docker exec -itu root gstt /usr/local/go/bin/go build -C /root/app -o /home/app/go-simple-tree -tags jsoniter
-docker exec -itu root gstt /bin/chown app:app /home/app/go-simple-tree
-docker exec -itu root gstt /bin/chmod +x /home/app/go-simple-tree
-docker exec -itu root gstt /usr/local/bin/atlas migrate apply --dir file:///root/app/ent/migrate/migrations --url mysql://du:pass@localhost:3306/testdb
+docker run --rm --name go-simple-tree-test -dv ".:/root/app:rw" -p 0:80 -p 0:3306 go-simple-tree
+docker exec -u root go-simple-tree-test /usr/local/go/bin/go test -C /root/app -tags jsoniter
+docker exec -u root go-simple-tree-test /usr/local/go/bin/go build -C /root/app -o /home/app/go-simple-tree -tags jsoniter
+docker exec -u root go-simple-tree-test /bin/chown app:app /home/app/go-simple-tree
+docker exec -u root go-simple-tree-test /bin/chmod +x /home/app/go-simple-tree
+docker exec -u root go-simple-tree-test /usr/local/bin/atlas migrate apply --dir file:///root/app/ent/migrate/migrations --url mysql://du:pass@localhost:3306/testdb
 
 # start test container
-docker exec -du app gstt /home/app/go-simple-tree
+docker exec -du app go-simple-tree-test /home/app/go-simple-tree
 
 # integration test using client
-docker exec -itu root gstt /usr/local/go/bin/go test -C /root/app/client
+docker exec -u root go-simple-tree-test /usr/local/go/bin/go test -C /root/app/client -v
+
+docker stop go-simple-tree-test
